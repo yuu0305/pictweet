7e2765fade77da56fa7a485f81beae963abf3dbc
yuu0305 <segyayuu8@gmail.com>
2019-07-01 18:55:35 +0900

    Add controller-testing
    コントローラーのテスト

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development, :test do
   gem 'jquery-rails'
   gem 'rspec-rails'
   gem 'factory_bot_rails'
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
-describe User  do
+
+describe User do
   describe '#create' do
+
     it "is invalid without a nickname" do
-      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
+
+    it "is invalid without a email" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
   end
 end
